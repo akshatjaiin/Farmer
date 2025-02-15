aea9ddc4da93143894d0a1c84526d68ccbae8878
Pravachan Patra <pravachanpatra@gmail.com>
2025-02-15 16:32:31 -0600

    name field of layout

--- backend/src/models/Layout.js
+++ backend/src/models/Layout.js
@@ -4,12 +4,14 @@ import EquipmentItemModel from "./EquipmentItem.js";
 
 
 const LayoutSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: true }, // need to get name textbox
     crop_areas: [{ type: mongoose.Schema.Types.ObjectId, ref: "CropArea" }],  // Use ObjectId with ref
     equipments: [{ type: mongoose.Schema.Types.ObjectId, ref: "EquipmentItem" }],  // Assuming EquipmentItem is a model
     total_cost: { type: Number, default: 0.0 },
+    total_area: { type: Number, default: 0.0 },
     width: { type: Number, required: true },
     height: { type: Number, required: true },
+   
 });
 
 const LayoutModel = mongoose.model("Layout", LayoutSchema);
