39bf33a5c962e1e2f2ccfe53452289efa8aefb1f
Colby-Frison <colbyfrison100@gmail.com>
2025-02-15 16:14:44 -0600

    Added equipment page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,19 @@ import { useState } from 'react'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LayoutPage from "./pages/LayoutPage";
-
+import EquipmentPage from "./pages/EquipmentPage";
+import './App.css';
 
 function App() {
-
   return (
     <Router>
-
-      <div>
+      <div className="app-container">
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
-
           <Route path="/layout-planning" element={<LayoutPage />} />
-
+          <Route path="/equipment" element={<EquipmentPage />} />
           <Route path="/register" element={<h1>Register</h1>} />
           <Route path="/login" element={<h1>Login</h1>} />
-          
-
         </Routes>
       </div>
     </Router>
