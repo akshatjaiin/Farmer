05f88a722c288b584108a7e3b63575636c6b6479
Pravachan Patra <pravachanpatra@gmail.com>
2025-02-15 18:01:54 -0600

    dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LayoutPage from "./pages/LayoutPage";
+import DashboardPage from "./pages/DashboardPage";
+
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
           <Route path="/" element={<h1>Home</h1>} />
 
           <Route path="/layout-planning" element={<LayoutPage />} />
+          <Route path="/layout-dashboard" element={<DashboardPage />} />
 
           <Route path="/register" element={<h1>Register</h1>} />
           <Route path="/login" element={<h1>Login</h1>} />
